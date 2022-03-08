0991b9d35efa8ea5689877df9d2362170db2e6de
Junwon_Park <upjw0429@gmail.com>
2022-03-08 17:47:04 +0900

    CSCenter 경로 수정

---
+++ src/main/java/com/mom/momhome/cscenter/CSCenterDto.java
@@ -0,0 +1,40 @@
+package com.mom.momhome.cscenter;
+
+public class CSCenterDto {
+	String cscenter_key="";
+	String user_key="";
+	String cscenter_title="";
+	String cscenter_contents="";
+	String cscenter_date="";
+	
+	public String getCscenter_key() {
+		return cscenter_key;
+	}
+	public void setCscenter_key(String cscenter_key) {
+		this.cscenter_key = cscenter_key;
+	}
+	public String getUser_key() {
+		return user_key;
+	}
+	public void setUser_key(String user_key) {
+		this.user_key = user_key;
+	}
+	public String getCscenter_title() {
+		return cscenter_title;
+	}
+	public void setCscenter_title(String cscenter_title) {
+		this.cscenter_title = cscenter_title;
+	}
+	public String getCscenter_contents() {
+		return cscenter_contents;
+	}
+	public void setCscenter_contents(String cscenter_contents) {
+		this.cscenter_contents = cscenter_contents;
+	}
+	public String getCscenter_date() {
+		return cscenter_date;
+	}
+	public void setCscenter_date(String cscenter_date) {
+		this.cscenter_date = cscenter_date;
+	}
+}

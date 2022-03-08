6c98cf81f9964f6f8a404205dc2e89ee3a94aca2
mail8617 <mail8617@naver.com>
2022-03-08 10:06:00 +0900

    team  - MembershipDao 추가

---
+++ src/main/java/com/mom/momhome/team/MembershipDao.java
@@ -0,0 +1,7 @@
+package com.mom.momhome.team;
+
+import com.mom.momhome.membership.MembershipDto;
+
+public interface MembershipDao {
+	void insertMembership(MembershipDto dto);
+}
---
+++ src/main/java/com/mom/momhome/team/MembershipDaoImpl.java
@@ -0,0 +1,20 @@
+package com.mom.momhome.team;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mom.momhome.membership.MembershipDto;
+
+@Repository("membershipDao")
+public class MembershipDaoImpl implements MembershipDao{
+
+	@Autowired
+	SqlSessionTemplate sm;
+	
+	@Override
+	public void insertMembership(MembershipDto dto) {
+		sm.insert("Membership_insert", dto);
+	}
+
+}
